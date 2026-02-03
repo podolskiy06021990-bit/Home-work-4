86c6f9564ab15f738c32c99681d469e96c991a27
Nikita Podolskiy <podolskiy06021990@gmail.com>
2026-02-03 11:45:52 +0300

    Add files via upload

---
+++ imdb_202602031144.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.imdb (genre,rating,runtime,film) VALUES
+	 ('fantasy',9.0,130,'Dungeon and Dragons'),
+	 ('history',7.0,120,'WW2'),
+	 ('comedy',5.0,90,'Zochan'),
+	 ('history',8.0,140,'WW1'),
+	 ('comedy',6.0,60,'Gilmore'),
+	 ('fantasy',9.0,110,'Startrec'),
+	 ('fantasy',9.0,130,'Dungeon and Dragons'),
+	 ('history',7.0,120,'WW2'),
+	 ('comedy',5.0,90,'Zochan'),
+	 ('history',8.0,140,'WW1');
+INSERT INTO public.imdb (genre,rating,runtime,film) VALUES
+	 ('comedy',6.0,60,'Gilmore'),
+	 ('fantasy',9.0,110,'Startrec');
